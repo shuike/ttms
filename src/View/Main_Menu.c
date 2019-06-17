cad198591c79d7501a9e8a1e20fd1049e35c834e
shuike <shuike007@126.com>
2019-06-17 10:45:45 +0800

    汉化

--- src/View/Main_Menu.c
+++ src/View/Main_Menu.c
@@ -18,17 +18,17 @@ void Main_Menu(void) {
 	char choice;
 	do { 
 		printf("\n==================================================================\n");
-		printf("**************** Theater Ticket Management System ****************\n");
-		printf("[S]tudio Management.演出厅管理\n");
-		printf("[P]lay Management.演出管理\n");
-		printf("[T]icket Sale.销售票\n");
-		printf("[R]eturn Ticket.退票\n");
-		printf("[Q]ueries查询\n");
-		printf("Ra[n]king and Statistics.统计\n");
-		printf("[A]ccount Management.账户管理\n");
-		printf("[E]xist.\n");
+		printf("**************** 剧院票务管理系统 ****************\n");
+		printf("[S]演出厅管理.\n");     /*管理演出厅，设置座位，管理剧目*/
+		printf("[P]演出管理.\n");       /*安排演出，生成演出票*/
+		printf("[T]售票.\n");
+		printf("[R]退票.\n");
+		printf("[Q]查询\n");            /*查演出，查演出票*/
+		printf("[n]排名和统计.\n");     /*统计销售额，统计票房*/
+		printf("[A]账户管理.\n");       /*个人资料，管理系统用户*/
+		printf("[E]退出.\n");
 		printf("\n==================================================================\n");
-		printf("Please input your choice:");
+		printf("请输入您的选择:");
 		fflush(stdin);
 		choice = getchar();
 		switch (choice) {
@@ -36,31 +36,30 @@ void Main_Menu(void) {
 		case 's':
 			Studio_UI_MgtEntry();
 			break;
-//		case 'P':
-//		case 'p':
-//			Play_UI_MgtEntry(0);//
-//			break;
-//		case 'Q':
-//		case 'q':
-//			Queries_Menu();//销售
-//			break;
-//		case 'T':
-//		case 't':
-//			Sale_UI_MgtEntry();//出售
-//			break;
-//		case 'R':
-//		case 'r':
-//			Sale_UI_ReturnTicket();//
-//			break;
-//		case 'N':
-//		case 'n':
-//			SalesAanalysis_UI_MgtEntry();
-//			break;
-//		case 'A':
-//		case 'a':
-//			Account_UI_MgtEntry();
-//			break;
+/*		case 'P':
+		case 'p':
+			Play_UI_MgtEntry(0);
+			break;
+		case 'Q':
+		case 'q':
+			Queries_Menu();
+			break;
+		case 'T':
+		case 't':
+			Sale_UI_MgtEntry();
+			break;
+		case 'R':
+		case 'r':
+			Sale_UI_ReturnTicket();
+			break;
+		case 'N':
+		case 'n':
+			SalesAanalysis_UI_MgtEntry();
+			break;
+		case 'A':
+		case 'a':
+			Account_UI_MgtEntry();
+			break;*/
 		}
 	} while ('E' != choice && 'e' != choice);
 }
-
